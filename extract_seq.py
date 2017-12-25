b9622e3150613b465a92a5cad3b55bad89713284
shexuan <shexuan523392@gmail.com>
2017-12-25 22:36:38 +0800

    debug...

--- extract_seq.py
+++ extract_seq.py
@@ -14,24 +14,23 @@ def extractSeq(id_file, fasta, result):
     '''extract sequence from fasta.'''
     flag = 0
     n = 0
-    with open(id_file) as seq_id, open(fasta) as fa, open(result, 'wt') as res:
-        id_list = (id_ for id_ in seq_id)
-        id_dict = dict.fromkeys(id_list, True)
+    with open(id_file) as seq_id, open(fasta) as fa, open(result, 'w') as res:
+        id_list = [id_.strip() for id_ in seq_id]
         for line in fa:
             if line.startswith('>'):
-                if line[1:] in id_dict:
-                    res.write(line)
-                    flag = 1
-                    n += 1
-                    del id_dict[line[1:]]
-                else:
-                    flag = 0
+                for id_ in id_list:
+                    if id_ in line:
+                        res.write(line)
+                        flag = 1
+                        n += 1
+                        # id_list.remove(id_) #如果id对应的序列唯一的话，取消此行注释可提高效率
+                        break
+                    else:
+                        flag = 0
             else:
                 if flag == 1:
                     res.write(line)
     print('over!\n{} sequences has been found!'.format(n))
-    for id_ in id_dict:
-        print('Cannot find ID:', id_)
 
 
 if __name__ == '__main__':
