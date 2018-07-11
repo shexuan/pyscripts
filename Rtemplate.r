55605a1c9d423b2d0599051164541b85f5696b58
shexuan <shexuan523392@gmail.com>
2018-07-11 13:47:02 +0800

    Rtemplate arg parse

---
+++ Rtemplate.r
@@ -0,0 +1,19 @@
+#!/bin/env Rscript
+# coding: utf-8
+
+
+#######################################################
+# Method 1
+args <- commandArgs()
+file <- args[6]
+
+# 第一个命令行输入的参数是args[6]，若想ars[1]表示第一个输入的参数，则使用commandArgs(trailling=TRUE)
+
+
+#######################################################
+# Method 2
+
+
+
+
+
