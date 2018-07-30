8ddc73354fcf74b10a128b23540bf16ff1ca1569
shexuan <shexuan523392@gmail.com>
2018-07-30 22:17:37 +0800

    debug and add api for outdir

--- vcfHandle/cluster_filtered.r
+++ vcfHandle/cluster_filtered.r
@@ -11,7 +11,10 @@ file <- args[6]
 method <- args[7]
 #file <- 'tmp2.cvf'
 
-vcf <- read.csv(file,sep='\t',stringsAsFactors=F)
+
+cols <- c('CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', 'SAMPLE', 'AC', 'AF', 'AN', 'BaseQRankSum', 'ClippingRankSum', 'DP', 'ExcessHet', 'FS', 'MLEAC', 'MLEAF', 'MQ', 'MQRankSum', 'QD', 'ReadPosRankSum', 'SOR', 'SAMPLE')
+#vcf <- read.csv(file,sep='\t',stringsAsFactors=F,header=T,col.names=cols)
+vcf <- read.csv(file,stringsAsFactors = F,header = F,sep='\t',col.names=cols ,skip = 1)
 vcf.no.NA <- dplyr::filter(vcf, FS!='NA' & 
                              MQ!='NA' &
                              MQRankSum!='NA' &
@@ -20,8 +23,8 @@ vcf.no.NA <- dplyr::filter(vcf, FS!='NA' &
                              SOR!='NA')
 
 # split the vcf to two dataframe, hete and homo according to columns AF
-vcf.no.NA.homo <- vcf.no.NA[vcf.no.NA$AF=='1.00',]
-vcf.no.NA.hete <- vcf.no.NA[vcf.no.NA$AF=='0.500',]
+vcf.no.NA.homo <- vcf.no.NA[vcf.no.NA$AF=='1',]
+vcf.no.NA.hete <- vcf.no.NA[vcf.no.NA$AF=='0.5',]
 
 # columns used for feature vectors 
 major.info <- c('FS','MQ','MQRankSum','QD','ReadPosRankSum','SOR')
@@ -45,7 +48,7 @@ if (method=='gmm'){
   gmm.hete <- Mclust(vcf.no.NA.hete[major.info], G=2)
   
   cls.homo <- as.vector(gmm.homo$classification)
-  cls.hete <- as.vector(gmm.homo$classification)
+  cls.hete <- as.vector(gmm.hete$classification)
   vcf.no.NA.hete$cluster <- cls.hete
   vcf.no.NA.homo$cluster <- cls.homo
   
@@ -59,9 +62,13 @@ vcf.no.NA.homo.filter <- vcf.no.NA.homo[vcf.no.NA.homo$cluster==positive.homo,]
 vcf.no.NA.hete.filter <- vcf.no.NA.hete[vcf.no.NA.hete$cluster==positive.hete,]
 
 
-# merge hete and homo into a dataframe
+# merge hete and homo into a dataframe and sort all variants by chrom and pos
 vcf.filtered.merge <- rbind(vcf.no.NA.hete.filter,vcf.no.NA.homo.filter)
-write.table(vcf.filtered.merge[seq(1,10)], file='cluster.filtered.vcf', sep='\t',row.names = F, col.names = F, quote = F)
+Chr <- c(paste('chr', seq(1,22), sep=''), c('chrX','chrY'))
+vcf.filtered.merge.sort <- data.frame()
+for (chr_ in Chr){
+  chr.sort <- arrange(vcf.filtered.merge[vcf.filtered.merge$CHROM==chr_,], POS)
+  vcf.filtered.merge.sort <- rbind(vcf.filtered.merge.sort, chr.sort)  
+} 
 
-
-
+write.table(vcf.filtered.merge.sort[seq(1,10)], file='cluster.filtered.vcf', sep='\t', row.names=F,col.names=F, quote=F)
--- vcfHandle/snp_filter.py
+++ vcfHandle/snp_filter.py
@@ -30,7 +30,8 @@ def filter_snp(raw_vcf, density_filtered="tmp1.vcf", step=200, limit=5):
             if line.startswith('#'):
                 res.write(line)
             else:
-                all_pos.setdefault(line.split('\t')[0], []).append(int(line.split('\t')[1]))
+                all_pos.setdefault(line.split('\t')[0], []).append(
+                    int(line.split('\t')[1]))
     for chr_, pos in all_pos.items():
         # 生成所有step区间
         start = pos[0]
@@ -43,7 +44,8 @@ def filter_snp(raw_vcf, density_filtered="tmp1.vcf", step=200, limit=5):
         distribution = [bisect(intervals, p) for p in pos]
         density = Counter(distribution)
         # interval 表示掉落在第几个区间，number表示掉落在这个区间的snp数量
-        filtered_snp = [interval for interval, number in density.items() if number <= limit]
+        filtered_snp = [interval for interval,
+                        number in density.items() if number <= limit]
         # distribution -> 所有snp位点的分布， filtered_snp -> 密度小于5的snp位点
         filter_dict[chr_] = (distribution, filtered_snp)
     raw_dict = OrderedDict()
@@ -63,9 +65,11 @@ def split_info(density_filtered):
     split the INFO column into 15 cols, and filtered some snp without "major info".
     whereafter clustered by Kmeans in R.
     '''
-    INFO = ['AC', 'AF', 'AN', 'BaseQRankSum', 'ClippingRankSum', 'DP', 'ExcessHet', 'FS', 'MLEAC', 'MLEAF', 'MQ', 'MQRankSum', 'QD', 'ReadPosRankSum', 'SOR']
-    header = '\t'.join((['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', 'son']+INFO))
-    major_info = ['FS', 'MQ', 'MQRankSum', 'QD', 'ReadPosRankSum', 'SOR']
+    INFO = ['AC', 'AF', 'AN', 'BaseQRankSum', 'ClippingRankSum', 'DP', 'ExcessHet',
+            'FS', 'MLEAC', 'MLEAF', 'MQ', 'MQRankSum', 'QD', 'ReadPosRankSum', 'SOR']
+    header = '\t'.join((['CHROM', 'POS', 'ID', 'REF', 'ALT',
+                         'QUAL', 'FILTER', 'INFO', 'FORMAT', 'SAMPLE']+INFO))
+    # major_info = ['FS', 'MQ', 'MQRankSum', 'QD', 'ReadPosRankSum', 'SOR']
     with open(density_filtered) as f, open('tmp2.vcf', 'w') as res:
         res.write(header+'\n')
         for line in f:
@@ -80,15 +84,16 @@ def split_info(density_filtered):
                 res.write(info_s + '\n')
 
 
-def cluster_filter(raw_vcf, algorithm):
+def cluster_filter(raw_vcf, algorithm, outdir):
     '''
     split all snp into two classes -- homo and hete. 
     And then clustering with Kmeans in R respectively. 
     Filtering the fake snp with the result of cluster.
     Finally merge them all.
     '''
-    os.system('Rscript /home/sxuan/dch/test/kmeans_cluster.r tmp2.vcf {}'.format(algorithm))
-    with open('density.cluster.filtered.vcf', 'w') as res, open('cluster.filtered.vcf') as f, open(raw_vcf) as raw:
+    os.system(
+        'Rscript /home/sxuan/dch/test/cluster_filtered.r tmp2.vcf {}'.format(algorithm))
+    with open(outdir+'/density.'+algorithm+'.filtered.vcf', 'w') as res, open('cluster.filtered.vcf') as f, open(raw_vcf) as raw:
         for line in raw:
             if line.startswith('#'):
                 res.write(line)
@@ -96,26 +101,31 @@ def cluster_filter(raw_vcf, algorithm):
                 break
         for line in f:
             res.write(line)
-    # os.remove('tmp1.vcf')
-    # os.remove('tmp2.vcf')
-    # os.remove('kmeans.filtered.vcf')
+    os.remove('tmp1.vcf')   # density filtered
+    os.remove('tmp2.vcf')   # the file split INFO
+    os.remove('cluster.filtered.vcf')  # clustered filtered
 
 
 def main(raw_vcf, algorithm):
     filter_snp(raw_vcf)
     density_filtered = 'tmp1.vcf'
     split_info(density_filtered)
-    cluster_filter(raw_vcf, algorithm)
+    cluster_filter(raw_vcf, algorithm, outdir)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Filtering snp with density and Kmeans cluster.")
-    parser.add_argument('--in', '-i', type=str, metavar='raw.vcf', help="raw input vcf file.")
+    parser = argparse.ArgumentParser(
+        description="Filtering snp with density and Kmeans cluster.")
+    parser.add_argument('--in', '-i', type=str,
+                        metavar='raw.vcf', help="raw input vcf file.")
     parser.add_argument('--algorithm', '-a', type=str, metavar='algorithm', default='kmeans',
                         choices=['gmm', 'kmeans'], help='cluster algorithm for filter variants. optional "gmm" or "kmeans".')
+    parser.add_argument('--outdir', '-o', default='.', metavar='outdir', help='Output directory')
     args = vars(parser.parse_args())
 
     algorithm = args['algorithm']
     raw_vcf = args['in']
-
+    outdir = args['outdir']
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
     main(raw_vcf, algorithm)
