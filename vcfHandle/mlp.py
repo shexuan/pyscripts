6f1d509cb88ed465994a87b32ab7b627ca7a87c9
shexuan <shexuan523392@gmail.com>
2018-08-03 18:36:57 +0800

    sort model rank

--- vcfHandle/mlp.py
+++ vcfHandle/mlp.py
@@ -6,6 +6,7 @@ from sklearn.externals import joblib
 import pandas as pd
 import numpy as np
 import os
+import argparse
 
 
 def mlp_classifier():
@@ -22,50 +23,90 @@ def mlp_classifier():
         for a in activation:
             for s in solver:
                 for alp in alpha:
-                    mlp = MLPClassifier(hidden_layer_sizes=h,
-                                        activation=a, solver=s, alpha=alp)
+                    mlp = MLPClassifier(hidden_layer_sizes=h, activation=a, solver=s, alpha=alp)
                     models.append(mlp)
     return models
 
-############# my data #################################
-# os.chdir('D:/smart-variant-filtering/mymodel/')
 
-# df = pd.read_csv('vcf.info', sep='\t', header=0)
+def test_model(features, tmp):
+    '''
+    Testing model with different parameters.
+    '''
+    df = pd.read_csv(features, header=0, sep='\t', usecols=(2, 3, 4, 5, 6, 7, 8, 9))
+    array = df.values
+    X = array[:, :-1]
+    Y = array[:, -1]
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=0.0, random_state=6)
+    kfold = model_selection.KFold(n_splits=10, random_state=6)
+
+    models = mlp_classifier()
+    res = open(tmp, 'wt', encoding='utf-8')
+    res.write('idx\tstd\tmean\tmodel\n')
+    for idx, model in enumerate(models):
+        cv_results = model_selection.cross_val_score(
+            model, X_train, Y_train.astype(int), cv=kfold, scoring='accuracy')
+        res.write('{idx}\t{std:0.2%}\t{mean:0.2%}\t{model}\n'.format(
+            idx=idx, std=cv_results.std(), mean=cv_results.mean(), model=model))
+    res.close()
+
+
+def sort_model(tmp, reslut):
+    '''
+    Sorting the model with different parameters according to accuracy.
+    '''
+    with open(tmp, 'r', encoding='utf-8') as f, open(result, 'wt', encoding='utf-8') as res:
+        try:
+            while True:
+                line = ''
+                for i in range(7):
+                    line += next(f).strip()
+                res.write(line+'\n')
+        except StopIteration:
+            pass
+
+
+def main():
+    test_model(features, tmp)
+    sort_model(tmp, result)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Train mlp parameters.")
+    parser.add_argument("--train_features", "-tf", type=str, help="Features tables used for trainning model.")
+    parser.add_argument("--outdir", '-o', type=str, help="Output directory.")
+    parser.add_argument("--param_prefix", "-pref", type=str, help="Model stat file name prefix.")
+    args = vars(parser.parse_args())
+
+    outdir = args['outdir']
+    prefix = args['param_prefix']
+    features = args['train_features']
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    tmp = outdir+'/'+'model_assess.tmp'
+    result = outdir+'/'+prefix+'_model_assess.txt'
+
+    main()
+
+# #################### SVF data #########################
+# df = pd.read_csv('HG002_oslo_exome_dbsnp_SNVs.table', sep='\t', header=0)
 # df = df.fillna(0.)
-
-# df['CLASS'] = df['cls'].apply(lambda x: int(1) if x == 'TP' else int(0))
-# df = df.drop(['CHROM', 'POS', 'cls', 'AF', 'AC', 'AN', 'ClippingRankSum',
-#               'ExcessHet', 'MLEAC', 'MLEAF', 'DP', 'BaseQRankSum'], axis=1)
+# df['CLASS'] = df['HG002.BD'].apply(lambda x: int(1) if x == 'TP' else int(0))
+# df = df.drop(['CHROM', 'POS', 'TYPE', 'HG002.BD'], axis=1)
 
 # array = df.values
-# X = array[:, 0:6]
-# Y = array[:, 6]
+# X = array[:, 0:7]
+# Y = array[:, 7]
 
+# validation_size = 0.
+# X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
+#     X, Y, test_size=validation_size, random_state=6)
+# kfold = model_selection.KFold(n_splits=5, random_state=6)
 
-#################### SVF data #########################
-df = pd.read_csv('HG002_oslo_exome_dbsnp_SNVs.table', sep='\t', header=0)
-df = df.fillna(0.)
-df['CLASS'] = df['HG002.BD'].apply(lambda x: int(1) if x == 'TP' else int(0))
-df = df.drop(['CHROM', 'POS', 'TYPE', 'HG002.BD'], axis=1)
-
-array = df.values
-X = array[:, 0:7]
-Y = array[:, 7]
-
-validation_size = 0.
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
-    X, Y, test_size=validation_size, random_state=6)
-kfold = model_selection.KFold(n_splits=5, random_state=6)
-
-models = mlp_classifier()
-print('model\tstd\tmean')
-res = open('model_assess.txt', 'wt', encoding='utf-8')
-for idx, model in enumerate(models):
-    cv_results = model_selection.cross_val_score(
-        model, X_train, Y_train.astype(int), cv=kfold, scoring='accuracy')
-    res.write('{idx}\t{std:0.2%}\t{mean:0.2%}\t{model}\n'.format(
-        idx=idx, std=cv_results.std(), mean=cv_results.mean(), model=model))
-
-
-res.close()
-print('training over')
+# models = mlp_classifier()
+# print('model\tstd\tmean')
+# res = open('model_assess.txt', 'wt', encoding='utf-8')
+# for idx, model in enumerate(models):
+#     cv_results = model_selection.cross_val_score(
+#         model, X_train, Y_train.astype(int), cv=kfold, scoring='accuracy')
+#     res.write('{idx}\t{std:0.2%}\t{mean:0.2%}\t{model}\n'.format(
+#         idx=idx, std=cv_results.std(), mean=cv_results.mean(), model=model))
